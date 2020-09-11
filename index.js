218ea9d17cf8e0a96fda018615eaed697467e9e5
Ashish Santha Kumar <ashishsanthakumar@Ashishs-MacBook-Pro.local>
2020-09-11 11:40:32 -0700

    Stage 5 Complete

--- index.js
+++ index.js
@@ -23,12 +23,11 @@ var color = d3.scaleOrdinal(d3.schemeCategory10)
 // append the svg object to the body of the page
 // append a g (group) element to 'svg' and
 // move the g element to the top+left margin
-var svg = d3.select("body").append("svg")
+var svg = d3.select(".center").append("svg")
                            .attr("width", width + margin.left + margin.right)
                            .attr("height", height + margin.top + margin.bottom)
                            .append("g")
-                           .attr("transform", `translate(${margin.left},${margin.top})`);
-                          
+                           .attr("transform", `translate(${margin.left},${margin.top})`);                 
 
 // Get the data
 d3.tsv("data/gapminderDataFiveYear.tsv").then(data => {
@@ -40,6 +39,10 @@ d3.tsv("data/gapminderDataFiveYear.tsv").then(data => {
         d.year = +d.year;
         d.pop = +d.pop;
     });
+  
+    var rad = d3.scaleLinear()
+        .domain([d3.min(data, function(d) { return d.pop; } ),d3.max(data, function(d) { return d.pop; } )])
+        .range([4,10]);    
 
     // Set scale domains based on the loaded data
     xScale.domain(d3.extent(data, function(d) { return d.gdpPercap; }));
@@ -50,13 +53,15 @@ d3.tsv("data/gapminderDataFiveYear.tsv").then(data => {
         .data([data])
         .attr("class", "line")
         .attr("d", valueline);
+
     
+
     // Add the scatterplot
     svg.selectAll("dot")
         .data(data)
         .enter().append("circle")
         .filter(function(d) { return d['year'] == 2007 || d['year'] == 1952;})
-        .attr("r", 5)
+        .attr("r", function(d) { return rad(d.pop); })
         .attr("cx", function(d) { return xScale(d.gdpPercap); })
         .attr("cy", function(d) { return yScale(d.lifeExp); })
         .style("fill", function(d) { return color(d.year); })
